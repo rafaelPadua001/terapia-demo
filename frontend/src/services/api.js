feca74722dac9f5698b142d8ef13ddabfea07b9a
Rafael Pádua <145411939+rafaelPadua001@users.noreply.github.com>
2026-05-03 21:02:31 -0300

    Fix
    Fix api call

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,19 @@
-import axios from "axios";
-import { useAuthStore } from "../store/auth";
-
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api"
-});
-
-api.interceptors.request.use((config) => {
-  const auth = useAuthStore();
-  if (auth.token) {
-    config.headers.Authorization = `Bearer ${auth.token}`;
-  }
-  return config;
-});
-
-export default api;
-
+import axios from "axios";
+
+import { useAuthStore } from "../store/auth";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "https://terapia-demo-1.onrender.com/api",
+});
+
+api.interceptors.request.use((config) => {
+  const auth = useAuthStore();
+
+  if (auth.token) {
+    config.headers.Authorization = `Bearer ${auth.token}`;
+  }
+
+  return config;
+});
+
+export default api;
